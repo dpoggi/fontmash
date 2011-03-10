d7d5fb7cf0e9983e93eef6a82a3b5b46340b1471
Dan Poggi <dan.poggi@gmail.com>
2011-03-10 18:29:13 -0500

    It mostly works! Need scroll support

--- public/javascripts/fontmash.js
+++ public/javascripts/fontmash.js
@@ -1,6 +1,12 @@
+function alignTables() {
+  var pos = $("table#first").offset();
+  pos.top -= 20;
+  $("table#second").css(pos);
+}
+
 function mashFonts() {
   if ($("input#font1").val() && $("input#font2").val()) {
-    $("div#first").css("font-family", $("input#font1").val());
-    $("div#second").css("font-family", $("input#font2").val());
+    $("table#first").css("font-family", $("input#font1").val());
+    $("table#second").css("font-family", $("input#font2").val());
   }
 }
