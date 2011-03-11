d11c4e8257763395a5edff3cb31d1f565f02c464
Dan Poggi <dan.poggi@gmail.com>
2011-03-10 19:34:26 -0500

    Some tweaks, realigns proper now

--- public/javascripts/fontmash.js
+++ public/javascripts/fontmash.js
@@ -1,12 +1,24 @@
-function alignTables() {
-  var pos = $("table#first").offset();
-  pos.top -= 20;
-  $("table#second").css(pos);
-}
-
-function mashFonts() {
-  if ($("input#font1").val() && $("input#font2").val()) {
-    $("table#first").css("font-family", $("input#font1").val());
-    $("table#second").css("font-family", $("input#font2").val());
+var FontMash = {
+  alignTables: function() {
+    var pos = $("table#first").offset();
+    pos.top -= 20;
+    $("table#second").css(pos);
+  },
+  mashFonts: function() {
+    if ($("input#font1").val() && $("input#font2").val()) {
+      $("table#first").css("font-family", $("input#font1").val());
+      $("table#second").css("font-family", $("input#font2").val());
+    }
+  },
+  swapFonts: function() {
+    var tmp = $("input#font1").val();
+    $("input#font1").val($("input#font2").val());
+    $("input#font2").val(tmp);
+    parent.mashFonts();
+  },
+  compatibility: function() {
+    $("input#font1").val("Arial");
+    $("input#font2").val("Times New Roman");
+    parent.mashFonts();
   }
-}
+};
